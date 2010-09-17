64242ecd3b4e04c4f7c00ab1d6184db3da7994fd
Tomas D'Stefano <tomasdestefi@gmail.com>
2010-09-17 16:20:32 -0300

    Put the system command for the Lib Notify - Linux Users

--- lib/infinity_test/notifications/lib_notify.rb
+++ lib/infinity_test/notifications/lib_notify.rb
@@ -1,7 +1,11 @@
 module InfinityTest
    module Notifications
      class LibNotify
-      
+       
+       def notify(options)
+         system "notify-send --expire-time 2000 --icon #{options[:image]} 'Ruby #{options[:title]}' '#{options[:message]}'"
+       end
+       
      end
    end
 end
