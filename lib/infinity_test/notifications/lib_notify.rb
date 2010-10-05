2822a49c59d563b59ff4d1fbc1685e3a1aedccd8
Tomas Destefi <tomasdestefi@gmail.com>
2010-10-05 16:41:40 -0300

    Begin to setup the expire time for lib_notfy users

--- lib/infinity_test/notifications/lib_notify.rb
+++ lib/infinity_test/notifications/lib_notify.rb
@@ -1,11 +1,16 @@
 module InfinityTest
    module Notifications
      class LibNotify
-       
-       def notify(options)
-         system "notify-send --expire-time 3000 --icon #{options[:image]} 'Ruby #{options[:title]}' '#{options[:message]}'"
+       attr_reader :expire_time
+
+       def initialize(hash={})
+         @expire_time = hash[:expire_time] || 100
        end
-       
+
+       def notify(options)
+         system "notify-send --urgency=normal --expire-time #{@expire_time} --icon #{options[:image]} 'Ruby #{options[:title]}' '#{options[:message]}'"
+       end
+
      end
    end
 end
--- spec/infinity_test/notifications/lib_notify_spec.rb
+++ spec/infinity_test/notifications/lib_notify_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 module InfinityTest
   module Notifications
     describe LibNotify do
-      
+
+      it 'should possible to set the expire time' do
+        LibNotify.new(:expire_time => 1200).expire_time.should equal 1200
+      end
+
+      it 'should set 500 miliseconds as default expire time' do
+        LibNotify.new.expire_time.should equal 100
+      end
+
     end
   end
 end
