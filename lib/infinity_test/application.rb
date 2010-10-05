1a887c05cd74cb44e8cf8db69c8bb5975d4f4f9b
Tomas Destefi <tomasdestefi@gmail.com>
2010-10-05 16:24:59 -0300

    Comment some methods in application object

--- lib/infinity_test/application.rb
+++ lib/infinity_test/application.rb
@@ -1,17 +1,17 @@
 module InfinityTest
   class Application
     include Notifications
-    
+
     attr_accessor :config, :library_directory_pattern
-    
-    # Initialize the Application object with the configuration instance to 
+
+    # Initialize the Application object with the configuration instance to
     # load configuration and set properly
     #
     def initialize
       @config = InfinityTest.configuration
       @library_directory_pattern = "^lib/*/(.*)\.rb"
     end
-    
+
     # Load the Configuration file
     #
     # Load first global file in => ~/.infinity_test
@@ -31,88 +31,111 @@ module InfinityTest
       load_global_configuration    # Separate global and local configuration
       load_project_configuration   # because it's more easy to test
     end
-    
+
+    # Return the sucess image to show in the notifications
+    #
     def sucess_image
       config.sucess_image
     end
-    
+
+    # Return the failure image to show in the notifications
+    #
     def failure_image
       config.failure_image
     end
-    
+
+    # Return the pending image to show in the notifications
+    #
     def pending_image
       config.pending_image
     end
-    
+
     # Return the block object setting in the config file
-    #    
+    #
     def before_callback
       config.before_callback
     end
-    
+
     # Return the block object setting in the config file
-    #    
+    #
     def before_environment_callback
       config.before_environment_callback
     end
-    
+
     # Return the block object setting in the config file
     #
     def after_callback
       config.after_callback
     end
-    
+
+    # Return the block object setting in the before(:each_ruby) block
+    #
     def before_each_ruby_callback
       config.before_each_ruby_callback
     end
-    
+
+    # Return the block object setting in the after(:each_ruby) block
+    #
     def after_each_ruby_callback
       config.after_each_ruby_callback
     end
-    
+
     # Return the rubies setting in the config file or the command line
     #
     def rubies
       config.rubies
     end
-    
+
+    # Return true if verbose mode is on
+    # Verbose mode is false as default
+    #
+    def verbose?
+      config.verbose
+    end
+
+    # Contruct all the commands for the test framework
+    #
     def construct_commands
       test_framework.construct_commands
     end
-    
+
+    # Return the test directory pattern setting in the .infinity_test file
+    #
     def test_directory_pattern
       test_framework.test_directory_pattern
     end
-    
+
     # Return a instance of the test framework class
     #
     def test_framework
       @test_framework ||= setting_test_framework
     end
-    
+
     # Return a instance of the Notification Framework class
     #
     def notification_framework
       @notification_framework ||= setting_notification
     end
-    
+
     def run_before_environment_callback!
       before_environment_callback.call(self) if before_environment_callback
     end
-    
+
     def run!(commands)
       before_callback.call if before_callback
-      
+
       commands.each do |ruby_version, command|
         call_each_ruby_callback(:before_each_ruby_callback, ruby_version)
         command = say_the_ruby_version_and_run_the_command!(ruby_version, command) # This method exist because it's more easier to test
         notify!(:results => command.results, :ruby_version => ruby_version)
         call_each_ruby_callback(:after_each_ruby_callback, ruby_version)
       end
-      
+
       after_callback.call if after_callback
     end
-    
+
+    # Send the message,image and the actual ruby version to show in the notification system
+    #
     def notify!(options)
       if notification_framework
         message = parse_results(options[:results])
@@ -121,11 +144,19 @@ module InfinityTest
         # skip(do nothing) when not have notification framework
       end
     end
-    
+
+    # Parse the results for each command to the test framework
+    #
+    # app.parse_results(['.....','108 examples']) # => '108 examples'
+    #
     def parse_results(results)
       test_framework.parse_results(results)
     end
 
+    # If the test pass, show the sucess image
+    # If is some pending test, show the pending image
+    # If the test fails, show the failure image
+    #
     def image_to_show
       if test_framework.failure?
         failure_image
@@ -133,41 +164,37 @@ module InfinityTest
         pending_image
       else
         sucess_image
-      end      
+      end
     end
-    
+
     def run_changed_lib_file(file)
       file = File.basename(file[1])
       files = test_framework.all_files.grep(/#{file}/i)
       run_commands_for_file(files.join(' ')) unless files.empty?
     end
-    
+
     def run_changed_test_file(file)
       run_commands_for_file(file)
     end
-    
+
     def run_commands_for_file(file)
       commands = test_framework.construct_commands(file)
-      run!(commands)      
+      run!(commands)
     end
-    
-    def verbose?
-      config.verbose
-    end
-    
+
     private
-    
+
     def call_each_ruby_callback(callback_type, ruby_version)
       callback = send(callback_type)
       callback.call(RVM::Environment.new(ruby_version)) if callback
     end
-    
+
     def say_the_ruby_version_and_run_the_command!(ruby_version, command)
       puts; puts "* { :ruby => #{ruby_version} }"
       puts command if verbose?
-      Command.new(:ruby_version => ruby_version, :command => command).run!      
+      Command.new(:ruby_version => ruby_version, :command => command).run!
     end
-    
+
     def setting_test_framework
       case config.test_framework
       when :rspec
@@ -176,7 +203,7 @@ module InfinityTest
         TestUnit.new :rubies => rubies
       end
     end
-    
+
     def setting_notification
       case config.notification_framework
       when :growl
@@ -189,11 +216,11 @@ module InfinityTest
     def load_global_configuration
       load_file(File.expand_path('~/.infinity_test'))
     end
-    
+
     def load_project_configuration
       load_file('./.infinity_test')
     end
-    
+
     def load_file(file)
       load(file) if File.exist?(file)
     end
