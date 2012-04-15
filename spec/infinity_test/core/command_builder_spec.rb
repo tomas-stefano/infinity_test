fe2de296444e6365dbd38b2a697cfcddb05659c2
Tomas D'Stefano <tomas_stefano@successoft.com>
2012-04-15 19:58:19 -0300

    Add method CommandBuilder#add.

--- spec/infinity_test/core/command_builder_spec.rb
+++ spec/infinity_test/core/command_builder_spec.rb
@@ -3,17 +3,27 @@ require 'spec_helper'
 module InfinityTest
   module Core
     describe CommandBuilder do
-      context 'constructing the command' do
-        it "should put space after every keyword" do
-          subject.bundle.exec.ruby.should == 'bundle exec ruby'
+      describe "#add" do
+        it "should add the argument in the command" do
+          subject.ruby.option(:S).add(:rspec).add(:spec).should == 'ruby -S rspec spec'
         end
+      end
 
+      describe "#option" do
         it "should put options with one dash and be possible to add more keywords" do
           subject.bundle.exec.ruby.option(:S).rspec.should == 'bundle exec ruby -S rspec'
         end
+      end
 
+      describe "#opt" do
         it "should put double dash options and be possible to add keywords" do
           subject.ruby.opt(:copyright).should == 'ruby --copyright'
+        end
+      end
+
+      describe '#method_missing' do
+        it "should put space after every keyword" do
+          subject.bundle.exec.ruby.some_file.should == 'bundle exec ruby some_file'
         end
       end
 
