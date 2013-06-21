af45acf38186e6c8f1dfe72b0f69a4c7ec3c4c44
Tomas D'Stefano <tomas_stefano@successoft.com>
2013-06-21 18:58:28 -0300

    Add doc comments in the watcher observer.

--- lib/infinity_test/observer/watchr.rb
+++ lib/infinity_test/observer/watchr.rb
@@ -7,14 +7,25 @@ module InfinityTest
         @observer = ::Watchr::Script.new
       end
 
+      # ==== Examples
+      #
+      #   watch('lib/(.*)\.rb') { |match_data| system("ruby test/test_#{match_data[1]}.rb") }
+      #   watch('test/test_helper.rb') { RunAll() }
+      #
       def watch(pattern_or_file, &block)
         @observer.watch(pattern_or_file.to_s, &block)
       end
 
+      # ==== Examples
+      #
+      #   watch_dir(:lib) { |file| RunTest(file) }
+      #
       def watch_dir(dir_name, extension = :rb, &block)
         @observer.watch("^#{dir_name}/*/(.*).#{extension}", &block)
       end
 
+      # Start the continuous test server.
+      #
       def start
         @handler = ::Watchr.handler.new
         @controller = ::Watchr::Controller.new(@observer, @handler)
