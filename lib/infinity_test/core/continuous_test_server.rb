27f5211863ac1274e72aa399f4d82baba9bf79ec
Tomas D'Stefano <tomas_stefano@successoft.com>
2013-07-14 19:00:11 -0300

    Start the continuous test server passing framework and observer objects.

--- lib/infinity_test/core/continuous_test_server.rb
+++ lib/infinity_test/core/continuous_test_server.rb
@@ -2,65 +2,58 @@ module InfinityTest
   module Core
     class ContinuousTestServer
       attr_reader :base
+      delegate :binary, :command_arguments, to: :test_framework
+      delegate :infinity_and_beyond, to: :base
 
       def initialize(base)
         @base = base
       end
 
       def start
-        # run_strategy
-        # base.start_observer
+        run_strategy
+        start_observer
       end
 
       # Run strategy based on the choosed ruby strategy.
       #
       def run_strategy
         # PENDING: run_before_callbacks
-        # strategy_instance.run
+        strategy.run
         # PENDING: run_after_callbacks
       end
 
       # Start to monitoring files in the project.
       #
       def start_observer
-        # if infinity_and_beyond.present?
-        #   add_heuristics
-        #   observer_instance.signal
-        #   observer_instance.start
-        # end
+        if infinity_and_beyond.present?
+          framework.heuristics
+          observer.start
+        end
       end
 
-      private
+      # Returns the instance for the configured strategy.
+      #
+      def strategy
+        @strategy ||= "InfinityTest::Strategy::#{base.strategy.to_s.classify}".constantize.new(self)
+      end
 
-      # # Adding heuristics based on the framework.
-      # #
-      # def add_heuristics
-      #   framework_instance.heuristics
-      # end
+      # Return a cached test framework instance.
       #
-      # # Returns the instance for the configured strategy.
-      # #
-      # def strategy_instance
-      #   InfinityTest::Strategy.const_get(base.strategy.to_s.classify).new(self)
-      # end
+      def test_framework
+        @test_framework ||= "::InfinityTest::TestFramework::#{base.test_framework.to_s.classify}".constantize.new
+      end
+
+      # Return a framework instance based on the base framework accessor.
       #
-      # # Return a cached test framework instance by the observer accessor.
-      # #
-      # def test_framework_instance
-      #   "::InfinityTest::TestFramework::#{test_framework.to_s.classify}".constantize.new
-      # end
+      def framework
+        @framework ||= "::InfinityTest::Framework::#{base.framework.to_s.camelize}".constantize.new(observer)
+      end
+
+      # Return a cached observer instance by the observer accessor.
       #
-      # # Return a framework instance based on the framework accessor.
-      # #
-      # def framework_instance
-      #   "::InfinityTest::Framework::#{framework.to_s.camelize}".constantize.new(self)
-      # end
-      #
-      # # Return a cached observer instance by the observer accessor.
-      # #
-      # def observer_instance
-      #   @observer_instance ||= "::InfinityTest::Observer::#{observer.to_s.classify}".constantize.new
-      # end
+      def observer
+        @observer ||= "::InfinityTest::Observer::#{base.observer.to_s.classify}".constantize.new
+      end
     end
   end
 end
--- spec/infinity_test/core/continuous_test_server_spec.rb
+++ spec/infinity_test/core/continuous_test_server_spec.rb
@@ -3,9 +3,66 @@ require 'spec_helper'
 module InfinityTest
   module Core
     describe ContinuousTestServer do
-      let(:continuous_test_server) { ContinuousTestServer.new(Core::Base) }
+      let(:base) { Core::Base }
+      let(:continuous_test_server) { ContinuousTestServer.new(base) }
 
       describe '#start' do
+        it 'run strategy, start observer' do
+          continuous_test_server.should_receive(:run_strategy)
+          continuous_test_server.should_receive(:start_observer)
+          continuous_test_server.start
+        end
+      end
+
+      describe '#start_observer' do
+        context 'when base configuration as infinity and beyond' do
+          before do
+            base.should_receive(:infinity_and_beyond).and_return(true)
+            base.stub(:framework).and_return(:rails)
+            base.stub(:observer).and_return(:watchr)
+          end
+
+          it 'add framework heuristics and start the observer' do
+            continuous_test_server.framework.should_receive(:heuristics)
+            continuous_test_server.observer.should_receive(:start)
+            continuous_test_server.start_observer
+          end
+        end
+
+        context 'when base configuration is not infinity and beyond' do
+          before { base.should_receive(:infinity_and_beyond).and_return(false) }
+
+          it 'do not start the observer' do
+            continuous_test_server.observer.should_not_receive(:start)
+            continuous_test_server.start_observer
+          end
+        end
+      end
+
+      describe '#strategy' do
+        subject { continuous_test_server.strategy }
+        before { base.should_receive(:strategy).and_return(:ruby_default) }
+
+        it { should be_instance_of InfinityTest::Strategy::RubyDefault }
+      end
+
+      describe '#test_framework' do
+        subject { continuous_test_server.test_framework }
+        before { base.should_receive(:test_framework).and_return(:rspec) }
+
+        it { should be_instance_of InfinityTest::TestFramework::Rspec }
+      end
+
+      describe '#binary' do
+        subject { continuous_test_server }
+
+        it { should respond_to :binary }
+      end
+
+      describe '#command_arguments' do
+        subject { continuous_test_server }
+
+        it { should respond_to :command_arguments }
       end
     end
   end
