be98075afa7c83aea4a0b699110f44d17101b49e
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-02-28 00:52:43 -0300

    Add the important methods to test unit works like rspec

--- lib/infinity_test/test_framework/test_unit.rb
+++ lib/infinity_test/test_framework/test_unit.rb
@@ -1,6 +1,17 @@
 module InfinityTest
   module TestFramework
     class TestUnit < Base
+      def test_dir
+        'test'
+      end
+
+      def test_helper_file
+        File.join(test_dir, 'test_helper.rb')
+      end
+
+      def binary
+        ''
+      end
     end
   end
 end
--- spec/infinity_test/test_framework/test_unit_spec.rb
+++ spec/infinity_test/test_framework/test_unit_spec.rb
@@ -4,6 +4,22 @@ module InfinityTest
   module TestFramework
     describe TestUnit do
       it_should_behave_like 'a infinity test test framework'
+
+      describe "#test_dir" do
+        it "should return spec as test dir" do
+          expect(subject.test_dir).to eq 'test'
+        end
+      end
+
+      describe "#test_helper_file" do
+        it "should be the spec helper" do
+          expect(subject.test_helper_file).to eq 'test/test_helper.rb'
+        end
+      end
+
+      describe '#binary' do
+        it 'should see the binary with the strategy instance'
+      end
     end
   end
 end
