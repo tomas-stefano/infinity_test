c863bf77af7332ef7fb59e16cfd3108582d252eb
Tomas D'Stefano <tomas_stefano@successoft.com>
2026-02-01 21:05:02 +0000

    Rename config file to INFINITY_TEST and complete test frameworks
    - Rename .infinity_test to INFINITY_TEST for clearer visibility
    - Complete TestUnit implementation with patterns for minitest output,
      success/failure/pending detection, test_dir setter, and .run? method
    - Add missing methods to RSpec: test_dir=, pending?, and .run?
    - Add comprehensive specs for TestUnit

--- spec/infinity_test/test_framework/test_unit_spec.rb
+++ spec/infinity_test/test_framework/test_unit_spec.rb
@@ -9,6 +9,26 @@ module InfinityTest
         it "returns test as test dir" do
           expect(subject.test_dir).to eq 'test'
         end
+
+        it "can be set to a custom directory" do
+          subject.test_dir = 'custom_test'
+          expect(subject.test_dir).to eq 'custom_test'
+        end
+      end
+
+      describe '#test_files' do
+        context 'when is empty' do
+          it 'returns the test dir' do
+            expect(subject.test_files).to eq 'test'
+          end
+        end
+
+        context 'when assign the test files' do
+          it 'returns the assigned value' do
+            subject.test_files = 'test/models/user_test.rb'
+            expect(subject.test_files).to eq 'test/models/user_test.rb'
+          end
+        end
       end
 
       describe "#test_helper_file" do
@@ -18,7 +38,155 @@ module InfinityTest
       end
 
       describe '#binary' do
-        it 'sees the binary with the strategy instance'
+        it 'returns ruby as binary' do
+          expect(subject.binary).to eq 'ruby'
+        end
+      end
+
+      describe '#test_message' do
+        subject(:test_unit) { TestUnit.new }
+
+        it 'returns the final test results' do
+          test_unit.test_message = "Finished in 0.001s, 1000.0 runs/s.\n10 runs, 15 assertions, 0 failures, 0 errors, 0 skips\n"
+          expect(test_unit.test_message).to eq '10 runs, 15 assertions, 0 failures, 0 errors, 0 skips'
+        end
+      end
+
+      describe '#success?' do
+        subject(:test_unit) { TestUnit.new }
+
+        context 'when test message has ZERO failures, ZERO errors, and ZERO skips' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 0 failures, 0 errors, 0 skips"
+          end
+
+          it 'returns true' do
+            expect(test_unit).to be_success
+          end
+        end
+
+        context 'when test message has ONE failure' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 1 failures, 0 errors, 0 skips"
+          end
+
+          it 'returns false' do
+            expect(test_unit).to_not be_success
+          end
+        end
+
+        context 'when test message has ONE error' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 0 failures, 1 errors, 0 skips"
+          end
+
+          it 'returns false' do
+            expect(test_unit).to_not be_success
+          end
+        end
+
+        context 'when test message has ONE skip' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 0 failures, 0 errors, 1 skips"
+          end
+
+          it 'returns false' do
+            expect(test_unit).to_not be_success
+          end
+        end
+      end
+
+      describe '#failure?' do
+        subject(:test_unit) { TestUnit.new }
+
+        context 'when test message has ONE failure' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 1 failures, 0 errors, 0 skips"
+          end
+
+          it 'returns true' do
+            expect(test_unit).to be_failure
+          end
+        end
+
+        context 'when test message has ONE error' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 0 failures, 1 errors, 0 skips"
+          end
+
+          it 'returns true' do
+            expect(test_unit).to be_failure
+          end
+        end
+
+        context 'when test message has ZERO failures and ZERO errors' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 0 failures, 0 errors, 0 skips"
+          end
+
+          it 'returns false' do
+            expect(test_unit).to_not be_failure
+          end
+        end
+      end
+
+      describe '#pending?' do
+        subject(:test_unit) { TestUnit.new }
+
+        context 'when test message has ONE skip' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 0 failures, 0 errors, 1 skips"
+          end
+
+          it 'returns true' do
+            expect(test_unit).to be_pending
+          end
+        end
+
+        context 'when test message has ZERO skips' do
+          before do
+            test_unit.test_message = "10 runs, 15 assertions, 0 failures, 0 errors, 0 skips"
+          end
+
+          it 'returns false' do
+            expect(test_unit).to_not be_pending
+          end
+        end
+      end
+
+      describe '.run?' do
+        context 'when test directory exists with test files' do
+          before do
+            allow(File).to receive(:exist?).with('test').and_return(true)
+            allow(File).to receive(:exist?).with('test/test_helper.rb').and_return(true)
+          end
+
+          it 'returns true' do
+            expect(TestUnit.run?).to be true
+          end
+        end
+
+        context 'when test directory exists with *_test.rb files' do
+          before do
+            allow(File).to receive(:exist?).with('test').and_return(true)
+            allow(File).to receive(:exist?).with('test/test_helper.rb').and_return(false)
+            allow(Dir).to receive(:[]).with('test/**/*_test.rb').and_return(['test/user_test.rb'])
+          end
+
+          it 'returns true' do
+            expect(TestUnit.run?).to be true
+          end
+        end
+
+        context 'when test directory does not exist' do
+          before do
+            allow(File).to receive(:exist?).with('test').and_return(false)
+          end
+
+          it 'returns false' do
+            expect(TestUnit.run?).to be false
+          end
+        end
       end
     end
   end
